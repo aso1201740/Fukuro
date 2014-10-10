8c9aa3cc7fbc5e1f0cb41934ce365b41414916fc
aso1201740 <1201740@st.asojuku.ac.jp>
2014-10-10 15:08:07 +0900

    dirから画像参照、表示

--- src/com/android/fukuro/MainActivity.java
+++ src/com/android/fukuro/MainActivity.java
@@ -1,9 +1,14 @@
 package com.android.fukuro;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,7 +32,39 @@ public class MainActivity extends Activity implements View.OnClickListener {
 		
         Button btnjump=(Button)findViewById(R.id.btnjump);
         btnjump.setOnClickListener(this);
-		
+        
+        FileOutputStream fo;
+
+        File newfile = new File("/data/data/com.android.fukuro/Item");
+        File newfile2 = new File("/data/data/com.android.fukuro/Thambnail");
+
+           if (newfile.mkdir()){
+             //System.out.println("ディレクトリの作成に成功しました");
+             Log.d("ファイル作成","ディレクトリの作成に成功しました");
+
+           }else{
+             //System.out.println("ディレクトリの作成に失敗しました");
+             Log.d("ファイル作成","ディレクトリの作成に失敗しました");
+           }
+
+           if (newfile2.mkdir()){
+             //System.out.println("ディレクトリの作成に成功しました");
+             Log.d("ファイル作成","ディレクトリの作成に成功しました");
+
+           }else{
+             //System.out.println("ディレクトリの作成に失敗しました");
+             Log.d("ファイル作成","ディレクトリの作成に失敗しました");
+           }
+
+           try{
+           File f = new File("/data/data/com.android.fukuro/maketest/test2.txt");
+               File parent = f.getParentFile();
+               if (parent != null && parent.canWrite()) { parent.mkdirs(); }
+               fo = new FileOutputStream(f);
+
+           } catch (IOException e) {
+               Log.e("text作成","text作成に失敗しました");
+           }
 	}
 	
 	@Override
--- src/com/android/fukuro/MyPage.java
+++ src/com/android/fukuro/MyPage.java
@@ -1,7 +1,13 @@
 package com.android.fukuro;
 
-	import android.content.Context;
+import java.io.File;
+
+import android.content.Context;
 import android.content.res.Resources;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -13,13 +19,24 @@ import android.widget.ImageView;
 	    /** Called when the activity is first created. */
 	    @Override
 	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        TextView tv = new TextView(this);
-	        tv.setText("This is SubActivity!");
-	        setContentView(tv);
-	        ImageView iv = new ImageView(this);
-	        iv.setImageResource(R.drawable.coordinate);
-	        setContentView(iv);
+	        super.onCreate(savedInstanceState); 
+	        
+	        setContentView(R.layout.mypage);
+	        
+	       // ImageView iv = new ImageView(this);
+	        //iv.setImageResource(R.drawable.coordinate);
+	        //setContentView(iv);
+	        
+	        File dir = new File("/data/data/com.android.fukuro/Item");
+	        if(dir.exists()){
+	            File file = new File(dir.getAbsolutePath()+"/coordinate.jpg");
+	            if (file.exists()) {
+	                    Bitmap _bm = BitmapFactory.decodeFile(file.getPath());
+	                    ((ImageView)findViewById(R.id.imageView1)).setImageBitmap(_bm); 
+	            }else{
+	                //存在しない
+	            }
+	        }      
 	    }    
 	}
 
